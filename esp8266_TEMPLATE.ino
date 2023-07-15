faaf1ff61acee20858d65a1b844dfff6522d4b6d
Sergio <harrizmat@mail.ru>
2023-07-15 17:58:56 +0300

    done
    esp8266 wifi client\ AP
    web interface with online NTP time
    automatic watering interface 
    all settings stored in EEPROM
    OTA firmware update

--- esp8266_TEMPLATE.ino
+++ esp8266_TEMPLATE.ino
@@ -1,13 +1,21 @@
-//***********// #define USEMEMORY
-//***********// #define ESP_WIFIAP // закомментировать если нужно  подключаться к существующей wifi
+//#define USEMEMORY  // использовать  EEPROM для сохранения настроек
+// #define ESP_WIFIAP //если нужно не подключаться к wifi а создать свою точку доступа 
 
 // Определяем точкy доступа
 #define WIFI_ACCES_POINT esp32              // имя и пароль создаваемой wifi сети
-#define WIFI_PASSWIRD 1234567812345678
-#define WIFI_IS_HIDDEN 1                        // 1 -скрытая точка, 0 - видимая
+#define WIFI_PASSWIRD 1234567890
+#define WIFI_IS_HIDDEN 0                        // 1 -скрытая точка, 0 - видимая
 #define CHANNEL 1                          // канал 1..11
 #define MAX_CONNECTION 4                   // макимальное количество подключений 1..8
-#define COLORTHEME "#40c2e6" //цвет кнопочек голубенький
+
+#define COLORTHEME "#40c2e6" //цвет кнопочек интерфейса голубенький
+
+//подключение релюшек включения воды
+#define TAP1 10
+#define TAP2 9
+#define TAP3 8
+#define TAP4 11
+
 
 // Подключем библиотеки
 #include <ESP8266WiFi.h>
@@ -18,7 +26,7 @@ GyverNTP ntp(5); // +5 GMT
 #include <EEManager.h>
 #include <GyverPortal.h>
 #include <LittleFS.h>
-GyverPortal ui(&LittleFS); // для проверки файлов
+GyverPortal ui(&LittleFS); 
 
 
 #ifdef ESP_WIFIAP
@@ -26,42 +34,44 @@ bool wifiHidden = WIFI_IS_HIDDEN;
 byte maxConnection = MAX_CONNECTION;
 bool wifiChanel = CHANNEL;
 const char* ssidAP = "esp";
-// const char* ssidAP = "esp";
-const char* passwordAP = "1234567812345678"; // пароль от 1 до 8 два раза
+const char* passwordAP = "1234567890"; // пароль от 1 до 0
 #else
 // если не точка, то wifi client
 const char* ssid = "kuso4ek_raya";
 const char* password = "1234567812345678";
+//const char* ssid = "esp32";
+//const char* password = "1234567890";
 #endif
 
 
-#define LED_PIN 2 // esp32 dev module LED_BUILTIN
+#define LED_PIN 5 // esp32 dev module LED_BUILTIN
 uint8_t state = 0; // автомат состояний
 bool sensor = 0;
-uint32_t ms = 0, prevMs = 0, stateMs = 0, sec = 1;
 GPtime upd_UpTime;
 unsigned int localPort = 2390;  // local port to listen for UDP packets
 
+uint32_t sec = 1;
 uint16_t realyear = 0;
 uint8_t nowhh = 0;
 uint8_t nowmm = 0;
 uint8_t nowss = 0;
 uint8_t uptimeHour = 0, uptimeMin = 0, uptimeSec = 0;
 
-uint16_t totalPhotos = 0;
+bool isWatering = 0; // флаг что сейчас полив происходит
+uint16_t wateringLeft = 0; // сколько еще поливаем на текущий момент
+
 // структура настроек
 struct Settings {
-  uint32_t shooterTime;
-  uint32_t afterSensorTime;
-  uint32_t sld;
-  uint32_t afterFlashDel;
-  char str[20];
-  bool wateringNow = 0;
+  bool autoWatering;
+  uint16_t wateringMin; // сколько минут поливать
+
+  uint8_t startMin; // начало полива
+  uint8_t startHour;
+
+  uint8_t stopMin;
+  uint8_t stopHour;
 };
 
-uint8_t startMin = 10;
-uint8_t startHour = 3;
-uint8_t wateringMin = 15;
 
 Settings set; // инициализация структуры типа mem
 
@@ -80,77 +90,75 @@ void ledBlink() {
 void webPageBuild() {
   GP.BUILD_BEGIN();
   GP.THEME(GP_DARK);
+  GP.UPDATE("uptimehh, uptimemm, nowhh, nowmm, nowss, wateringLeft, isWatering");// какие поля нужно обновлять
 
-  GP.UPDATE("uptimehh, uptimemm, uptimess, nowhh, nowmm, nowss");// какие поля нужно обновлять
-
-  // обновление случайным числом
   GP.TITLE("Меню");
-  GP.HR();
-  GP.BUTTON("btn", "Shot", "", COLORTHEME); // "" - id компонента
-  //-  GP.LABEL("Всего:");
-  //-  GP.LABEL("NAN", "label1");
-  //-  GP.LABEL("фоток");
-  //-  GP.BREAK();
-  //-  GP.LABEL("Сфоткал");
-  //-  GP.LABEL("NAN", "label2");
-  //-  GP.LABEL("сек назад");
-  GP.BREAK();
+  GP.BREAK(); GP.HR(); GP.HR();
+
   GP.LABEL("Аптайм:");
   GP.LABEL("uptimehh", "uptimehh");
   GP.LABEL("ч");
   GP.LABEL("uptimemm", "uptimemm");
   GP.LABEL("м");
   GP.BREAK();
-
   GP.LABEL("Время:");
   GP.LABEL("nowhh", "nowhh");
   GP.LABEL(":");
   GP.LABEL("nowmm", "nowmm");
   GP.LABEL(" :");
   GP.LABEL("nowss", "nowss");
-  GP.BREAK();
+  GP.BREAK(); GP.HR();
 
-  GP.HR();
-  //GP.SLIDER("sld", set.sld);
-  //GP.BREAK();
-  //GP.TEXT("txt", "", set.str);
-
-  GP.LABEL("Задержка датчика");
-  GP.NUMBER("uiPhotoGap", "number", set.afterSensorTime); GP.BREAK();
-  GP.LABEL(" мс");
+  // GP.BUTTON("btn", "Полить сейчас", "", COLORTHEME); // "" - id компонента
   GP.BREAK();
-  GP.HR();
-  GP.LABEL("Задержка вспышки ");
-  GP.NUMBER("uiFlashDel", "number", set.afterFlashDel); GP.BREAK();
-  GP.LABEL(" мс");
-  GP.BREAK();
-  GP.HR();
-  GP.LABEL("Удержание затвора");
-  GP.NUMBER("uiShooterTime", "number", set.shooterTime); GP.BREAK();
-  GP.LABEL("в милисекундах");
-  GP.BREAK();
-  GP.HR();
+  GP.LABEL("Полив");
+  GP.SWITCH("isWatering", isWatering, COLORTHEME); //
 
+  GP.LABEL("еще ");
+  GP.SELECT("wateringLeft", "0,1,2,3,4,5,6,7,8,9,10,15,20,\
+  30,40,50,60,70,80,90,100,120,140,160,180,200,250,300", wateringLeft);
+  GP.LABEL("минут.");
+  GP.BREAK();  GP.HR();
 
-  GP.SELECT("startHour", "0,1,2,3,4,5,6,7,8,9,10,11,12,", startHour);
-  GP.SELECT("startMin", "0,1,2,3,4,5,6,7,8,9,10,11,12,\
+  GP.LABEL("Автополив деревьев");
+  GP.SWITCH("treeWatering", set.autoWatering, COLORTHEME); //
+  GP.BREAK();
+  GP.LABEL("Начать полив");
+  /// начало полива
+  GP.SELECT("startHour", "00,01,02,03,04,05,06,07,08,09,10,11,12,\
+  13,14,15,16,17,18,19,20,21,22,23", set.startHour);
+  GP.SELECT("startMin", "00,01,02,03,04,05,06,07,08,09,10,11,12,\
   13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,\
   28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,\
-  43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,0", startMin);
-  GP.SELECT("wateringMin", "0,1,2,3,4,5,6,7,8,9,10,11,12,\
+  43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,0", set.startMin);
+  GP.BREAK();
+
+  GP.LABEL("Поливать");
+  GP.SELECT("wateringMin", "1,2,3,4,5,6,7,8,9,10,15,20,\
+  30,40,50,60,70,80,90,100,120,140,160,180,200,250,300", set.wateringMin);
+  GP.LABEL("минут.");
+
+/*
+  GP.LABEL("Окончить полив");
+  GP.SELECT("stopHour", "00,01,02,03,04,05,06,07,08,09,10,11,12,\
+  13,14,15,16,17,18,19,20,21,22,23", set.stopHour);
+  GP.SELECT("stopMin", "00,01,02,03,04,05,06,07,08,09,10,11,12,\
   13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,\
   28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,\
-  43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,0", wateringMin);
-  GP.BREAK();
+  43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,0", set.stopMin);
+ */ 
+  
+  GP.BREAK(); GP.HR(); GP.BREAK();
 
-  GP.RELOAD_CLICK("idx");
 
-  GP.BREAK();
-  GP.HR();
 
-  GP.SWITCH("treeWatering", 1, COLORTHEME);
+  // вызов алерта по кнопке
+  GP.ALERT("alert_saved", "Настройки сохранены"); // выскакивает по нажатию на кнопку
+  GP.BUTTON_MINI("saveBtn", "Сохранить настройки", "", COLORTHEME); // "" - id компонента
+  GP.UPDATE_CLICK("alert_saved", "saveBtn");
+  GP.BREAK(); GP.BREAK(); GP.HR(); GP.HR();
 
-  GP.BREAK();
+  // загрузка прошивки
   String s;
   s += F("<a href='");
   s += "/ota_update";
@@ -167,45 +175,47 @@ void webPageBuild() {
 void webPageAction() {
 
   if (ui.update()) {
-    //-    ui.updateInt("label1", totalPhotos);
-    //-    ui.updateInt("label2", sec);
-    ui.updateInt("uptimehh", uptimeHour);
+    ui.updateInt("uptimehh", uptimeHour); // аптайм
     ui.updateInt("uptimemm", uptimeMin);
-    ui.updateInt("nowhh", nowhh);
+
+    ui.updateInt("nowhh", nowhh); // текущее время
     ui.updateInt("nowmm", nowmm);
     ui.updateInt("nowss", nowss);
+
+    ui.updateInt("wateringLeft", wateringLeft);
+
+    //алерт - настройки сохранены
+    if (ui.update("alert_saved")) {
+#ifdef USEMEMORY
+      memory.update(); //обновление настроек в EEPROM памяти
+      ui.answer(1); // показать алерт (текст у него задан в конструкторе)
+#endif
+    }
   }//update()
+
   if (ui.click()) {
     Serial.println("UI CLICK detected");
-    // по нажатию на кнопку сфотографируем
-    if (ui.click("btn")) ledBlink();
-    // перезапишем в shooterTime что ввели в интерфейсе
-    if (ui.clickInt("uiPhotoGap", set.afterSensorTime)) {
-      Serial.print("set.afterSensorTime: ");
-      Serial.println(set.shooterTime);
-#ifdef USEMEMORY
-      memory.update(); //обновление настроек в EEPROM памяти
-#endif
+    // переключатель автополива
+    if (ui.clickBool("treeWatering", set.autoWatering)) {
     }
-    if (ui.clickInt("uiFlashDel", set.afterFlashDel)) {
-      Serial.print("set.afterFlashDel: ");
-      Serial.println(set.afterFlashDel);
-#ifdef USEMEMORY
-      memory.update(); //обновление настроек в EEPROM памяти
-#endif
+    //включение полива прямо сейчас
+    if (ui.clickBool("isWatering", isWatering)) {
     }
-    if (ui.clickInt("uiShooterTime", set.shooterTime)) {
-      Serial.print("set.shooterTime: ");
-      Serial.println(set.shooterTime);
-#ifdef USEMEMORY
-      memory.update(); //обновление настроек в EEPROM памяти
-#endif
-    }
-    ui.clickInt("startMin", startMin);  // поймать и записать индекс
-    ui.clickInt("startMin", startHour);  // поймать и записать индекс
-    ui.clickInt("wateringMin", wateringMin);  // поймать и записать индекс
 
+    // по нажатию на кнопку
+    if (ui.click("btn")) {
+      ledBlink();
+    }
+
+    ui.clickInt("startMin", set.startMin);  // поймать и записать индекс
+    ui.clickInt("startHour", set.startHour);  // поймать и записать индекс
+    ui.clickInt("wateringMin", set.wateringMin);  // поймать и записать индекс
+    ui.clickInt("stopMin", set.stopMin);  // поймать и записать индекс
+    ui.clickInt("stopHour", set.stopHour);  // поймать и записать индекс
+
+    ui.clickInt("wateringLeft", wateringLeft);  // поймать и записать индекс
   }//click()
+
 }//webPageAction()
 
 
@@ -213,7 +223,7 @@ void webPageAction() {
 void webUI_Init() {
   ui.attachBuild(webPageBuild);
   ui.attach(webPageAction);
-  ui.start();
+  ui.start(); //  ui.start("esp");
   ui.enableOTA();   // без пароля
   if (!LittleFS.begin()) Serial.println("FS Error");
   ui.downloadAuto(true);
@@ -222,101 +232,176 @@ void webUI_Init() {
 
 void wifiKeep() {
 
-#ifdef ESP_WIFIAP
-  // Инициируем точку доступа WiFi
-
-  WiFi.softAP(ssidAP, passwordAP, wifiChanel, wifiHidden, maxConnection);
-  // IPAddress myIP = WiFi.softAPIP();
-  Serial.print("wifi_AP IP: ");
-  Serial.println(WiFi.softAPIP());
-#else
-  if  (WiFi.status() == WL_CONNECTED) return;
-  else {
-    // Подключаемся к Wi-Fi
-    Serial.print("try conn to ");
-    Serial.print(ssid);
-    Serial.print(":");
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    byte i = 0;
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-      i++;
-      if (i > 40) ESP.restart();
+  static uint8_t sw = 0; // автомат
+  static uint8_t i = 0; // cчетчик
+  static uint32_t prevMs = 0;
+  /* debug
+    static uint8_t prevsw = 0; // автомат
+    if (sw != prevsw ){
+      prevsw = sw;
+      Serial.print("sw = ");
+      Serial.println(sw);
     }
-    Serial.print("Connected. \nIP: ");
-    // Выводим IP ESP
-    Serial.println(WiFi.localIP());
-  }
+  */
+  switch (sw) {
+    case 0:
+      sw = 3;
+      prevMs = millis();
+      break;
+    // инициализация подключения
+    case 3:
+      if (millis() - prevMs >= 200L) {
+        prevMs = millis();
+#ifdef ESP_WIFIAP
+        // Инициируем точку доступа WiFi
+        WiFi.softAP(ssidAP, passwordAP, wifiChanel, wifiHidden, maxConnection);
+        // IPAddress myIP = WiFi.softAPIP();
+        Serial.print("Wifi access point started. IP: ");
+        Serial.println(WiFi.softAPIP());
+        sw = 10; // на повтор инициализации
+#else
+        if  (WiFi.status() != WL_CONNECTED) {
+          Serial.println();
+          Serial.print(millis() / 1000);
+          Serial.print(" Try conn wifi to "); Serial.print(ssid); Serial.print(":");
+          WiFi.mode(WIFI_STA);
+          WiFi.begin(ssid, password);
+          sw = 5; // проверяем подключение
+        } else sw = 10; // на повтор инициализации
 #endif
-}//wifiInit()
+      }//if ms
+      break;
+    // пока не подключится, пробуем переподключаться
+    case 5:
+      if (millis() - prevMs >= 1000L) {
+        prevMs = millis();
+        if  (WiFi.status() != WL_CONNECTED) {
+          Serial.print(".");
+          i++;
+          // после 10 попыток на отдых
+          if (i > 20) {
+            i = 0;
+            // if (i > 40) ESP.restart();
+            sw = 10; // нет подключения, ждем 20 секунд
+          }
+        }
+        else {
+          Serial.print("Connected. \nIP: ");
+          Serial.println(WiFi.localIP());
+          sw = 10;
+        }
+      }//ms
+      break;
+    // длительное ожидание и повторная проверка подключения
+    case 10:
+      if (millis() - prevMs >= 20000L) {
+        prevMs = millis();
+        sw = 3;
+      }
+      break;
+  }//switch(sw)
+}//wifiKeep()
+
 
 void NTP_Init() {
+  //  ntp.setHost("ntp1.stratum2.ru");
   ntp.begin();
   delay(300);
-  if (!ntpUpdate()) Serial.println("ntp can't update");
-
 }
 
-bool ntpUpdate() {
-  realyear = ntp.year();
-  for (int i = 0; i < 10; i++) {
-    byte err = ntp.status();
-    if (!err && (realyear > 2022) && (realyear < 2040)) {
-      //if ((realyear > 2022) && (realyear < 2035)) {
-      nowss = ntp.second();
-      nowmm = ntp.minute();
-      nowhh =  ntp.hour();
-      return 1;
-    } else  {
-      Serial.printf("year: %lu \t", realyear);
-      Serial.printf("ntp error: %lu \n", err);
-    }
-  }//for
-  return 0;
-  //uint8_t realday = ntp.day();
-  //uint8_t realmonth = ntp.month();
-  //uint16_t realyear = ntp.year();
-  //uint8_t realdayweek = ntp.dayWeek();
-  //String timeString();            // получить строку времени формата ЧЧ:ММ:СС
-  //String dateString();            // получить строку даты формата ДД.ММ.ГГГГ
-  //uint8_t ntp.status();               // получить статус системы
+
+void ntpUpdate() {
+  static uint8_t sw = 0; // автомат
+  static uint32_t prevMs = 0;
+  static byte err = 0;
+  /* debug
+    static uint8_t prevsw = 0; // автомат
+    if (sw != prevsw ) {
+     prevsw = sw;
+     Serial.print("sw = "); Serial.println(sw);
+    } */
+  if (WiFi.status() == WL_CONNECTED) {
+    ntp.tick();
+    switch (sw) {
+      case 0:
+        sw = 5;
+        prevMs = millis();
+        break;
+      case 5:
+        if (millis() - prevMs >= 5000L) {
+          prevMs = millis();
+          err = ntp.status();
+          realyear = ntp.year();
+          if (!err && (realyear > 2022) && (realyear < 2040)) {
+            //if ((realyear > 2022) && (realyear < 2035)) {
+            nowss = ntp.second();
+            nowmm = ntp.minute();
+            nowhh =  ntp.hour();
+            sw = 10;
+          } else  {
+            Serial.printf("year: %lu \t", realyear);
+            Serial.printf("ntp error: %lu \n", err);
+            //sw = 3;
+            sw = 5;
+          }
+        }//ms
+        break;
+      //ждем час
+      case 10:
+        if (millis() - prevMs >= 3600000L) {
+          prevMs = millis();
+          sw = 5;
+        }
+        break;
+
+        //uint8_t realday = ntp.day();
+        //uint8_t realmonth = ntp.month();
+        //uint16_t realyear = ntp.year();
+        //uint8_t realdayweek = ntp.dayWeek();
+        //String timeString();            // получить строку времени формата ЧЧ:ММ:СС
+        //String dateString();            // получить строку даты формата ДД.ММ.ГГГГ
+        //uint8_t ntp.status();               // получить статус системы
 
 
-  // 0 - всё ок
-  // 1 - не запущен UDP
-  // 2 - не подключен WiFi
-  // 3 - ошибка подключения к серверу
-  // 4 - ошибка отправки пакета
-  // 5 - таймаут ответа сервера
-  // 6 - получен некорректный ответ сервера}//ntpUpdate()
+        // 0 - всё ок
+        // 1 - не запущен UDP
+        // 2 - не подключен WiFi
+        // 3 - ошибка подключения к серверу
+        // 4 - ошибка отправки пакета
+        // 5 - таймаут ответа сервера
+        // 6 - получен некорректный ответ сервера}//ntpUpdate()
+    }//switch(sw)
+  }//if wifi status == con
 }//ntpUpdate()
 
 
 void pinsBegin() {
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 0);
+  pinMode(TAP1, OUTPUT);
+  digitalWrite(TAP1, 0);
 }  //pinsBegin()
 
+
 void times_tick() {
+  static uint32_t prevMs = 0, stateMs = 0;
+
   // инкрементируем Аптайм
-  if ((ms - prevMs) > 1000L) {
-    prevMs = ms;
+  if ((millis() - prevMs) > 1000L) {
+    prevMs = millis();
     sec++;
     uptimeSec++;
     nowss++;
     //Serial.print("state:");
     //Serial.println(state);
     // обонвлять ntp пока не получим нормальный год
-    if((realyear < 2022) || (realyear > 2040)) ntpUpdate();
+    if ((realyear < 2022) || (realyear > 2040)) ntpUpdate();
     if (uptimeSec > 59) {
       uptimeSec = 0;
       uptimeMin++;
       if (uptimeMin > 59) {
         uptimeMin = 0;
         uptimeHour++;
-        if (!ntpUpdate()) Serial.println("ntp can't update");
         Serial.printf("%lu uptime hours %lu mins\n", uptimeHour, uptimeMin);
       }
     }  //if sec
@@ -330,16 +415,28 @@ void times_tick() {
         Serial.printf("%lu real hours %lu mins\n", nowhh, nowmm);
       }
     }  //if sec
-
   }//ms 1000
-}//times_tick()
+}// times_tick()
+
+
+void checkloop() {
+  // put your main code here, to run repeatedly:
+  static uint32_t prevMs = 0;
+  static uint16_t loopMs = 0;
+  loopMs = millis() - prevMs;
+  if (loopMs > 5) {
+    Serial.print("loop = ");
+    Serial.println(loopMs );
+  }
+  prevMs = millis();
+}//checkloop()
 
 
 void setup() {
   // put your setup code here, to run once:
   // Инициируем последовательный порт
   Serial.begin(115200);
-  Serial.println("\n\n\n\n");
+  Serial.println("\n\n\n");
   EEPROM.begin(100);  // выделить память (больше или равно размеру даты)
   memory.begin(0, 'a');
   pinsBegin();
@@ -349,12 +446,10 @@ void setup() {
 }//setup
 
 void loop() {
-  // put your main code here, to run repeatedly:
   wifiKeep(); yield();
   ui.tick();  yield();
-  ntp.tick(); yield();
+  // ntpUpdate();  yield();
   memory.tick();  yield();
   times_tick();  yield();
-  ms = millis();
 
 }//loop
